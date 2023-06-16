99ef972739848f83b9cda7056b388683924c2217
Laura Hall <podinjshmi1993@gmail.com>
2023-06-16 10:50:02 +0100

    challage one is done

---
+++ challenges/check-if-equal.js
@@ -0,0 +1,19 @@
+
+
+function isEqual(numOne, numTwo){
+    if(numOne === numTwo){
+        return true;
+    }else
+    return false;
+
+}
+
+
+
+
+//test case 
+console.log(isEqual(4, 4), "should be True");
+console.log(isEqual(0, 0), "should be True");
+console.log(isEqual(-4, -4), "should be True");
+console.log(isEqual(4, -4), "should be False");
+console.log(isEqual(0, 1), "should be False");
